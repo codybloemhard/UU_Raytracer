93f099a3e001028652759b07149c5eaf2abedaff
Egor Dmitriev <egordmitriev2@gmail.com>
2018-04-30 18:58:52 +0200

    Added scene object

---
+++ Graphics/Scene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Template
+{
+    /// <summary>
+    /// Scene object contains a hierarchy of objects and a camera
+    /// </summary>
+    public class Scene : IRenderable
+    {
+        public List<Object> Objects { get; set; }
+        public Camera CurrentCamera { get; set; }
+
+        public Scene(Camera camera) : this(camera, new List<Object>()) {}
+
+        public Scene(Camera camera, List<Object> objects)
+        {
+            CurrentCamera = camera;
+            this.Objects = objects;
+        }
+
+        public void Render(Matrix4 origin)
+        {
+            CurrentCamera.Clear();
+            var m = CurrentCamera.TransformMatrix(origin);
+            var candidates = CurrentCamera.Cull(Objects);
+            foreach (var candidate in candidates) {
+                candidate.Render(m);
+            }
+        }
+    }
+}
