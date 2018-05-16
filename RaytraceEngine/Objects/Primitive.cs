7b625506fbf5455f57a7c5890a5c7e378b5fc83b
Erwin Glazenburg <erwin.glazenburg@gmail.com>
2018-05-16 09:49:15 +0200

    Triangle

--- RaytraceEngine/Objects/Primitive.cs
+++ RaytraceEngine/Objects/Primitive.cs
@@ -127,4 +127,68 @@ namespace RaytraceEngine.Objects
             return new Vector2(Vector3.Dot(hit.Position, u), Vector3.Dot(hit.Position, v));
         }
     }
+
+    public class Triangle : Primitive
+    {
+        public Vector3 Normal { get; private set; }
+        private Vector3[] vertices = new Vector3[3];
+        public Vector3[] Vertices
+        {
+            get { return vertices; }
+            set
+            {
+                vertices = value;
+                UpdateNormal();
+            }
+        }
+
+        public void UpdateNormal()
+        {
+            Vector3 AB, BC;
+            AB = vertices[0] - vertices[1];
+            BC = vertices[1] - vertices[2];
+            Normal = Vector3.Cross(AB, BC).Normalized();
+        }
+
+        public override bool CheckHit(Ray ray, out RayHit hit)
+        {
+            float eps = 0.000001f;
+            hit = new RayHit();
+            Vector3 edge1, edge2, h, s, q;
+            float a, f, u, v;
+            edge1 = vertices[1] - vertices[0];
+            edge2 = vertices[2] - vertices[0];
+            h = Vector3.Cross(ray.Direction, edge2);
+            a = Vector3.Dot(edge1, h);
+            if (a > -eps && a < eps) return false;
+            f = 1 / a;
+            s = ray.Origin - vertices[0];
+            u = f * Vector3.Dot(s, h);
+            if (u < 0 || u > 1) return false;
+            q = Vector3.Cross(s, edge1);
+            v = f * Vector3.Dot(ray.Direction, q);
+            if (v < 0 || v > 1) return false;
+
+            if (u + v > 1) return false;
+
+            float t = f * Vector3.Dot(edge2, q);
+            if(t > eps)
+            {
+                hit.Position = ray.Origin + ray.Direction * t;
+                hit.Distance = t;
+                if (Vector3.Dot(Normal, ray.Direction) < 0)
+                    hit.Normal = Normal;
+                else hit.Normal = -Normal;
+                hit.HitObject = this;
+                return true;
+            }
+            return false;
+        }
+        
+
+        public override Vector2 GetUV(RayHit hit)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
